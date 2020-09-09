6a15dca070c0c59674d41a84e8773d87f254e545
fan <fan@itjiyun.com>
2020-09-09 21:53:17 +0800

    第二天

---
+++ 项目/leyou/leyou/leyou-item/leyou-item-service/src/main/java/cn/rain/LeyouItemServiceApplication.java
@@ -0,0 +1,16 @@
+package cn.rain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("cn.rain.item.mapper") // mapper接口的包扫描
+public class LeyouItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouItemServiceApplication.class, args);
+    }
+}
---
+++ 项目/leyou/leyou/leyou-item/leyou-item-service/src/main/java/cn/rain/item/mapper/CategoryMapper.java
@@ -0,0 +1,7 @@
+package cn.rain.item.mapper;
+
+import cn.rain.item.pojo.Category;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CategoryMapper extends Mapper<Category> {
+}
